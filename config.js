1ad8027b93bf70a1fcb3866cd577f4c7438d6094
aSerheieva <a.serheieva@andersenlab.com>
2018-02-07 17:36:05 +0200

    fix 1

--- config.js
+++ config.js
@@ -17,18 +17,5 @@ exports.config = {
                 savePath: './report'
             })
         );
-
-        let AllureReporter = require('jasmine-allure-reporter');
-        jasmine.getEnv().addReporter(new AllureReporter({
-            resultsDir: 'allure-results'
-        }));
-        jasmine.getEnv().afterEach(function(done){
-            browser.takeScreenshot().then(function (png) {
-                allure.createAttachment('Screenshot', function () {
-                    return new Buffer(png, 'base64')
-                }, 'image/png')();
-                done();
-            })
-        })
     }
 };
